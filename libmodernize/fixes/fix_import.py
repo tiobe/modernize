3d431c0070cbd4197073fc677029d484deac57c4
Maikel Steneker <maikel.steneker@tiobe.com>
2019-11-04 17:42:02 +0100

    Fixed SyntaxError.
    This error was the following:
      File "./libmodernize/fixes/fix_import.py", line 25
    SyntaxError: Non-ASCII character '\xe2' in file ./libmodernize/fixes/fix_import.py on line 25, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details
    This was caused by introducing non-ASCII quotes.

--- libmodernize/fixes/fix_import.py
+++ libmodernize/fixes/fix_import.py
@@ -22,6 +22,6 @@ class FixImport(fix_import.FixImport):
         # If there are any non-future imports, add absolute_import
         # Causes many files to get changed, updated and checked-in with new version unneededly.
         # When doing the clean-up after having moved to Python3, the same files will have to get changed,
-        # updated and checked-in with new version again, just to have the “from __future__ import absolute_import” removed.
+        # updated and checked-in with new version again, just to have the "from __future__ import absolute_import" removed.
         #libmodernize.add_future(node, 'absolute_import')
         return super(FixImport, self).transform(node, results)
